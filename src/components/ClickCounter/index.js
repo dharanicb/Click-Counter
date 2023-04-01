653ac6b1a669c9edc3bc51f0ff6eb086a8b687fd
dharanicb <dharanicb86391@gmail.com>
2023-04-01 14:19:49 +0000

    solution recent

--- src/components/ClickCounter/index.js
+++ src/components/ClickCounter/index.js
@@ -6,15 +6,17 @@ import './index.css'
 
 class ClickCounter extends Component {
   state = {count: 0}
+
   onIncrement = () => {
     this.setState(prevState => ({count: prevState.count + 1}))
   }
+
   render() {
     const {count} = this.state
     return (
       <div className="container">
         <h1 className="count">
-          The Button has been clicked <span className="value">{count}</span>
+          The Button has been clicked <span className="value">{count} </span>
           times
         </h1>
         <p>Click the button to increase the count</p>
